b04ceac8e9069e1d7e0f48cbe99875be1dd7ecef
csnirs <walsh.jthomas@outlook.com>
2023-08-31 00:26:50 +0100

    Second commit, added comments and changed variable names.

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ int[] arr = {3, 1, 2, 4, 3};
 Console.WriteLine($"Result: {MinimumDifference(arr)}");
 
 public static partial class Program {
+    /// <summary>
+    /// Find the minimum difference possible between the sums of two
+    /// non-empty sub arrays of the given array. 
+    /// In this solution, the sum of elements of the argument array is 
+    /// calculated.
+    /// The left hand sum from the first index (0) for each possible 
+    /// partition is calculated, and the right hand sum is taken as the
+    /// difference between the left hand sum and the total sum. 
+    /// The minimum difference is updated on each iteration and returned. 
+    /// </summary>
+    /// <param name="arr"></param>
+    /// <returns></returns>
     public static int MinimumDifference(int[] arr) {
         int[] sums  = new int[(arr.Length - 1)];
 
@@ -16,12 +28,15 @@ public static partial class Program {
         }
 
         // Produce the left hand sums 
+        // The corresponding right hand sum (rhs) is:
+        //      rhs = total_sum - lhs
+        // If abs(lhs - rhs) < minimum_difference then update difference 
         int minDiff = arrSum; 
-        int runningSum = 0; 
+        int lhs = 0; 
         for (int i = 0; i < (arr.Length - 1); i++) {
-            runningSum += arr[i];
+            lhs += arr[i];
             minDiff = 
-                Math.Min(minDiff, (Math.Abs(runningSum - (arrSum - runningSum))));
+                Math.Min(minDiff, (Math.Abs(lhs - (arrSum - lhs))));
         }
 
         return minDiff;
