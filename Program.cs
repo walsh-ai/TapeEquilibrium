4539d3e4eeaa914f1d4df72f84372c26540cdea9
csnirs <walsh.jthomas@outlook.com>
2023-08-31 00:14:40 +0100

    First commit, built solution.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Top level statements
+// Test solution 
+using System.Globalization;
+
+int[] arr = {3, 1, 2, 4, 3}; 
+Console.WriteLine($"Result: {MinimumDifference(arr)}");
+
+public static partial class Program {
+    public static int MinimumDifference(int[] arr) {
+        int[] sums  = new int[(arr.Length - 1)];
+
+        // Sum the whole array
+        int arrSum = 0;
+        for (int i = 0; i < arr.Length; i++) {
+            arrSum += arr[i];
+        }
+
+        // Produce the left hand sums 
+        int minDiff = arrSum; 
+        int runningSum = 0; 
+        for (int i = 0; i < (arr.Length - 1); i++) {
+            runningSum += arr[i];
+            minDiff = 
+                Math.Min(minDiff, (Math.Abs(runningSum - (arrSum - runningSum))));
+        }
+
+        return minDiff;
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Tape Equilibrium problem.
+
+Array of N integers A represents numbers on a tape. 
+
+Any integer P such that 0 < P < N splits the tape into two non-empty parts (split the array into two): 
+A[0], A[1], ..., A[P-1]
+A[P], A[P+1], ..., A[N - 1] 
+
+The difference between the two parts is defined as the absolute difference between the sums of the elements of the two parts. 
+
+Given a non-empty array, return the minimal difference that can be achieved by splitting the array at point P. 
